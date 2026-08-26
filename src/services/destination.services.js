a7aaab7c61e384f070051fbee63d616a621ca5a9
Rahul Vishwakarma <rahulrjp16@gmail.com>
2026-08-26 14:37:30 +0530

    package and destination endpoints updated

--- src/services/destination.services.js
+++ src/services/destination.services.js
@@ -1,6 +1,6 @@
 import api from "../config/axios.config"
 
 export const getDestinations = async () => {
-    const res = await api.get('/destinations');
+    const res = await api.get('/destination');
     return res.data;
 }
--- src/services/packages.services.js
+++ src/services/packages.services.js
@@ -1,11 +1,11 @@
 import api from "../config/axios.config"
 
 export const getAllPackages = async () => {
-    const res = await api.get("/packages");
+    const res = await api.get("/package");
     return res.data;
 }
 
 export const getPackageById = async (id) => {
-    const res = await api.get(`/packages/${id}`);
+    const res = await api.get(`/package/${id}`);
     return res.data;
 }
